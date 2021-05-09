db7e16255012127e70d3c013b850306edc84e5b2
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-05-09 12:22:00 +0530

    authentication state manage

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -1,18 +1,16 @@
 import 'package:anybuy/provider/AuthData_Provider.dart';
+import 'package:anybuy/screens/UserHome.dart';
 import 'package:anybuy/screens/merchScreen.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../screens/UserHome.dart';
-import 'package:flutter/material.dart';
-
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final authData = Provider.of<AuthData>(context);
     authData.getCurrentUserData();
-    if (authData.currentUserData["isMerchant"] == true &&
-        authData.auth.currentUser != null) {
+    if (authData.currentUserData.isNotEmpty &&
+        authData.currentUserData["isMerchant"] == true) {
       return MerchScreen();
     }
     return UserHome();
