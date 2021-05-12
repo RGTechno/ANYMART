e9aaf7807e325b628212aa6ffe06008eb53816ea
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-05-12 13:16:42 +0530

    modified profile section confirm delete

--- lib/screens/Profile_Screen.dart
+++ lib/screens/Profile_Screen.dart
@@ -51,6 +51,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
     final authData = Provider.of<AuthData>(context);
     final currentUserData = authData.currentUserData;
 
+    Future _deleteAlert() {
+      return showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text("ARE YOU SURE?"),
+          content: Text(
+            "Do you really want to delete your account?\nThis action can't be undo.",
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(ctx).pop(false);
+              },
+              child: Text("No"),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(ctx).pop(true);
+                setState(() {
+                  _isLoading = true;
+                });
+                if (authData.currentUserData["isMerchant"] == true) {
+                  await authData.deleteMerchantAccount();
+                } else {
+                  await authData.deleteUserAccount();
+                }
+                setState(() {
+                  _isLoading = false;
+                });
+                await Navigator.of(context).pushReplacementNamed(homeScreen);
+              },
+              child: Text("Yes"),
+            ),
+          ],
+        ),
+      );
+    }
+
     void _updateDetails() async {
       if (!_profileFormKey.currentState.validate()) {
         print("Invalid");
@@ -91,180 +129,175 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   ),
                 ],
               ),
-              body: SingleChildScrollView(
-                child: Form(
-                  key: _profileFormKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Form(
+                      key: _profileFormKey,
+                      child: Column(
                         children: [
-                          Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 5.0,
-                                vertical: 10,
+                          Row(
+                            children: [
+                              Flexible(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 5.0,
+                                    vertical: 10,
+                                  ),
+                                  child: TextFormField(
+                                    focusNode: _firstName,
+                                    decoration:
+                                        inpDec("firstname", "firstname"),
+                                    initialValue:
+                                        "${currentUserData["firstname"]}",
+                                    onFieldSubmitted: (String value) {
+                                      firstName = value;
+                                      _firstName.unfocus();
+                                      FocusScope.of(context)
+                                          .requestFocus(_lastName);
+                                    },
+                                    onSaved: (String updatedValue) {
+                                      setState(() {
+                                        firstName = updatedValue;
+                                      });
+                                    },
+                                  ),
+                                ),
                               ),
-                              child: TextFormField(
-                                focusNode: _firstName,
-                                decoration: inpDec("firstname", "firstname"),
-                                initialValue: "${currentUserData["firstname"]}",
-                                onFieldSubmitted: (String value) {
-                                  firstName = value;
-                                  _firstName.unfocus();
-                                  FocusScope.of(context)
-                                      .requestFocus(_lastName);
-                                },
-                                onSaved: (String updatedValue) {
-                                  setState(() {
-                                    firstName = updatedValue;
-                                  });
-                                },
+                              Flexible(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 5.0,
+                                    vertical: 10,
+                                  ),
+                                  child: TextFormField(
+                                    focusNode: _lastName,
+                                    decoration: inpDec("lastname", "lastname"),
+                                    initialValue:
+                                        "${currentUserData["lastname"]}",
+                                    onFieldSubmitted: (String value) {
+                                      lastName = value;
+                                      _lastName.unfocus();
+                                      FocusScope.of(context)
+                                          .requestFocus(_phone);
+                                    },
+                                    onSaved: (String updatedValue) {
+                                      setState(() {
+                                        lastName = updatedValue;
+                                      });
+                                    },
+                                  ),
+                                ),
                               ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 5.0,
+                              vertical: 10,
+                            ),
+                            child: TextFormField(
+                              focusNode: _phone,
+                              decoration: inpDec("Phone", "Phone No."),
+                              initialValue:
+                                  currentUserData["phoneNumber"] != null
+                                      ? "${currentUserData["phoneNumber"]}"
+                                      : "",
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return "Field is Required";
+                                }
+                                if (value.length != 10) {
+                                  return "Invalid, Requires 10 Digit valid number";
+                                }
+                                return null;
+                              },
+                              keyboardType: TextInputType.number,
+                              onFieldSubmitted: (String value) {
+                                phone = value;
+                                _phone.unfocus();
+                                FocusScope.of(context).requestFocus(_location);
+                              },
+                              onSaved: (String updatedValue) {
+                                setState(() {
+                                  phone = updatedValue;
+                                });
+                              },
                             ),
                           ),
-                          Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 5.0,
-                                vertical: 10,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 5.0,
+                              vertical: 10,
+                            ),
+                            child: TextFormField(
+                              focusNode: _location,
+                              decoration: inpDec(
+                                  "Set Default Home location", "location"),
+                              initialValue: currentUserData["location"] != null
+                                  ? "${currentUserData["location"]}"
+                                  : "",
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return "Field is Required";
+                                }
+                                return null;
+                              },
+                              onFieldSubmitted: (String value) {
+                                location = value;
+                                _location.unfocus();
+                                FocusScope.of(context).requestFocus(_update);
+                              },
+                              onSaved: (String updatedValue) {
+                                setState(() {
+                                  location = updatedValue;
+                                });
+                              },
+                            ),
+                          ),
+                          OutlinedButton.icon(
+                            focusNode: _update,
+                            style: ButtonStyle(
+                              padding:
+                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                EdgeInsets.symmetric(horizontal: 20),
                               ),
-                              child: TextFormField(
-                                focusNode: _lastName,
-                                decoration: inpDec("lastname", "lastname"),
-                                initialValue: "${currentUserData["lastname"]}",
-                                onFieldSubmitted: (String value) {
-                                  lastName = value;
-                                  _lastName.unfocus();
-                                  FocusScope.of(context).requestFocus(_phone);
-                                },
-                                onSaved: (String updatedValue) {
-                                  setState(() {
-                                    lastName = updatedValue;
-                                  });
-                                },
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  side: BorderSide(
+                                    color: Colors.black54,
+                                  ),
+                                ),
                               ),
                             ),
+                            onPressed: _updateDetails,
+                            icon: Icon(
+                              Icons.save_outlined,
+                            ),
+                            label: Text("Update"),
                           ),
                         ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 5.0,
-                          vertical: 10,
-                        ),
-                        child: TextFormField(
-                          focusNode: _phone,
-                          decoration: inpDec("Phone", "Phone No."),
-                          initialValue: currentUserData["phoneNumber"] != null
-                              ? "${currentUserData["phoneNumber"]}"
-                              : "",
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Field is Required";
-                            }
-                            if (value.length != 10) {
-                              return "Invalid, Requires 10 Digit valid number";
-                            }
-                            return null;
-                          },
-                          keyboardType: TextInputType.number,
-                          onFieldSubmitted: (String value) {
-                            phone = value;
-                            _phone.unfocus();
-                            FocusScope.of(context).requestFocus(_location);
-                          },
-                          onSaved: (String updatedValue) {
-                            setState(() {
-                              phone = updatedValue;
-                            });
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 5.0,
-                          vertical: 10,
-                        ),
-                        child: TextFormField(
-                          focusNode: _location,
-                          decoration:
-                              inpDec("Set Default Home location", "location"),
-                          initialValue: currentUserData["location"] != null
-                              ? "${currentUserData["location"]}"
-                              : "",
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return "Field is Required";
-                            }
-                            return null;
-                          },
-                          onFieldSubmitted: (String value) {
-                            location = value;
-                            _location.unfocus();
-                            FocusScope.of(context).requestFocus(_update);
-                          },
-                          onSaved: (String updatedValue) {
-                            setState(() {
-                              location = updatedValue;
-                            });
-                          },
-                        ),
-                      ),
-                      OutlinedButton.icon(
-                        focusNode: _update,
-                        style: ButtonStyle(
-                          padding:
-                              MaterialStateProperty.all<EdgeInsetsGeometry>(
-                            EdgeInsets.symmetric(horizontal: 20),
-                          ),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              side: BorderSide(
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ),
-                        ),
-                        onPressed: _updateDetails,
-                        icon: Icon(
-                          Icons.save_outlined,
-                        ),
-                        label: Text("Update"),
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.red,
-                          ),
-                        ),
-                        onPressed: () async {
-                          setState(() {
-                            _isLoading = true;
-                          });
-                          if (authData.currentUserData["isMerchant"] == true) {
-                            await authData.deleteMerchantAccount();
-                          } else {
-                            await authData.deleteUserAccount();
-                          }
-                          setState(() {
-                            _isLoading = false;
-                          });
-                          await Navigator.of(context)
-                              .pushReplacementNamed(homeScreen);
-                        },
-                        child: Text(
-                          "DELETE ACCOUNT",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.red,
+                      ),
+                    ),
+                    onPressed: _deleteAlert,
+                    child: Text(
+                      "DELETE ACCOUNT",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
               ),
             ),
           );
